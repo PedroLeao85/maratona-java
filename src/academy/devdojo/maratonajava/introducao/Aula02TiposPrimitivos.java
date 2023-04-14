a92cdcb8e493eccc477cffceff47089044241b17
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-04-14 16:13:34 -0300

    Cria Classe Aula02TiposPrimitivos

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, shoet, long, boolean
+        int idade = 10;
+        System.out.println(idade);
+        System.out.println("A idade é: " + idade + "anos");
+
+
+    }
+}
