f09ff2a65974566de70d6f14b0d8039509c908b4
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-04-28 13:56:57 -0300

    Cria variáveis

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -2,11 +2,20 @@ package academy.devdojo.maratonajava.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        // int, double, float, char, byte, shoet, long, boolean
+        // int, double, float, char, byte, short, long, boolean
+
         int idade = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
         System.out.println(idade);
         System.out.println("A idade é: " + idade + "anos");
-
 
     }
 }
