e336d3686e58711c57fa747cd73315d67e5e983d
PedroLeao85 <phalves1@hotmail.com>
2023-05-02 22:41:33 -0300

    Create Aula3TiposPrimitivosExercicios.java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula3TiposPrimitivosExercicios.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula3TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String nome = "Jose";
+        String endereco = "Rua 123";
+        double salário = 2.5000;
+        String dataRecebimentoSalario = "02/05/2023";
+        String relatorio = "Eu " + nome +  ", morando no endereço " + endereco + " , confirmo que recebi " +
+                "o salário de R$" + salário + " , na data " + dataRecebimentoSalario;
+
+        //System.out.println("Eu " + nome +  ", morando no endereço " + endereco + " , confirmo que recebi " +
+          //      "o salário de R$" + salário + " , na data " + dataRecebimentoSalario);
+
+        System.out.println(relatorio);
+
+    }
+
+}
