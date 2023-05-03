0393621a84624ba71ecc66e4d453b70de36c2963
PedroLeao85 <phalves1@hotmail.com>
2023-05-02 22:15:14 -0300

    Adiciona variável String

--- src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -3,22 +3,28 @@ package academy.devdojo.maratonajava.introducao;
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
+        // casting: força a entrada de um valor
+        //String não é tipo primitivo, string é uma classe
 
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        int idade = (int) 10000000000L;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0D;
         byte idadeByte = 10;
-        short idadeShort = 10;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = '\u0041';
+        String nome = "Nicko";
+        var nome2 = "Mcbrain";
 
         System.out.println(idade);
         System.out.println("A idade é: " + idade + "anos");
+        System.out.println(salarioFloat);
         System.out.println(verdadeiro);
         System.out.println(falso);
         System.out.println("char " + caractere);
+        System.out.println("Oi, meu nome é " + nome + "!!");
 
     }
 }
