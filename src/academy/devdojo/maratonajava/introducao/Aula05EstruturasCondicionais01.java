028dcb689af3f4dc3b981a1e03f7fd629c2829d9
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-06-02 16:39:04 -0300

    Adiciona Classe EstruturasCondicionais02

--- src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
 
     public static void main(String[] args) {
         int idade = 15;
@@ -9,7 +9,11 @@ public class Aula05EstruturasCondicionais {
         //operador de negação !
         if(isAutorizadoComprarBebida){
             System.out.println("Autorizado comprar bebida alcoolica");
+        }else{
+            System.out.println("Não autorizado comprar bebida alcoolica");
         }
+
+
 
         if(!isAutorizadoComprarBebida){
             System.out.println("Não autorizado comprar bebida alcoolica");
