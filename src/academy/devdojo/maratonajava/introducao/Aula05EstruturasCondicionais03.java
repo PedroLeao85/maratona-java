a490ba61bfa9e71b44171214644e93c4bfc8a819
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-06-05 16:50:24 -0300

    Adiciona classe Aula05EstruturasCondicionais03

---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        //Doar se salario for maior que 5000
+        //operador ternário: (condicao) ? verdadeiro : falso
+
+        double salario  = 6000;
+        String mensagemDoar = "Eu vou doar 500 pro DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condições, mas vou ter";
+        String resultado = salario > 5000 ? mensagemDoar : mensagemNaoDoar;
+
+
+        System.out.println(resultado);
+    }
+}
