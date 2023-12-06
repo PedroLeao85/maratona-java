47d7380bcb45a935cc4c65fb70371e22ad8f0f03
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2023-12-06 13:44:56 -0300

    Adiciona Classe Aula07Arrays01

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays01 {
+    public static void main(String[] args) {
+        //
+
+        int [] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 11;
+        idades[2] = 15;
+
+        for (int count : idades){
+            System.out.println(count);
+        }
+
+
+
+
+    }
+}
