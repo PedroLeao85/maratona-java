6733673bd05687c80927069f27c838c64e30c341
Pedro Leão <63025279+PedroLeao85@users.noreply.github.com>
2024-04-16 11:12:19 -0300

    Create Aula07Arrays02.java

---
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arrays02.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula07Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int long, float e double o valor é 0
+        //char
+        //boolean false
+        //String null
+        int [] idades = new int[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+
+        booel [] idades1 = new char[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+
+        float [] idades2 = new float[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+
+        String [] idades3 = new String[3];
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
